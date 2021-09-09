6ab6ccc7244621aa2f480d6ad1954629aaa92339
santosh <santoshvkawale123@gmail.com>
2021-09-09 12:26:43 +0530

    Update PQR.java

--- XYZ/src/ABC/PQR.java
+++ XYZ/src/ABC/PQR.java
@@ -5,6 +5,7 @@ public class PQR {
 	public static void main(String[] args) {
 		System.out.println("This is our  second peoject");
 		System.out.println("This is our  second peoject");
+		System.out.println("This is our  second peoject");
 
 	}
 
