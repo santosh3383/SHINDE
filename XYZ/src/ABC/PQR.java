947e6b184297d5d2ade3e2a3ac3554ee6fe8ab95
santosh <santoshvkawale123@gmail.com>
2021-09-09 12:36:24 +0530

    CHANGES DONE
    PLEASE REVIEW

--- XYZ/src/ABC/PQR.java
+++ XYZ/src/ABC/PQR.java
@@ -5,7 +5,9 @@ public class PQR {
 	public static void main(String[] args) {
 		System.out.println("This is our  second peoject");
 		System.out.println("This is our  second peoject");
-		System.out.println("This is oursecond peoject");
+		System.out.println("This is our second peoject");
+		System.out.println("This is our second peoject");
+		System.out.println("This is our second peoject");
 
 	}
 
